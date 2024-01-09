5aa67da69c1db7c543f863a00683943841d2e00e
Hayesone <joehayesone@gmail.com>
2024-01-09 17:07:58 +0000

    Base classes added for Rover and Plateau

---
+++ Plateau.cs
@@ -0,0 +1,9 @@
+class Plateau {
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public Plateau(int width, int height) {
+        Width = width;
+        Height = height;
+    }
+}
---
+++ Rover.cs
@@ -0,0 +1,11 @@
+public class Rover {
+    public int X {get; set;}
+    public int Y {get; set;}
+    public char Direction {get;set;} // Only N/E/S/W
+
+    public Rover(int x, int y, char direction) {
+        X = x;
+        Y = y;
+        Direction = direction;
+    }
+}
